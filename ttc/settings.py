21df4ab41c1d6bbc6fa4f80e18687a62337a0405
KhadishaKenzhebekkyzy <khadisha.kenzhebekkyzy@nu.edu.kz>
2020-07-30 16:47:50 +0600

    debug

--- ttc/settings.py
+++ ttc/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '17i^kgb0bp(+(%occe-r(#-))euzh3go-y_+b(-ibslla16x*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['transtelecom.herokuapp.com', '127.0.0.1']
 
